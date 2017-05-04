99a5ab7ed7280afa194862fd2f38520bc22ad63c
mehulrao <mehulrao@gmail.com>
2017-05-04 19:34:04 -0400

    Removed Switch

--- Gandalf-Instructions-OBJC/SecondViewController.m
+++ Gandalf-Instructions-OBJC/SecondViewController.m
@@ -10,7 +10,6 @@
 
 @interface SecondViewController ()
 
-@property (weak, nonatomic) IBOutlet UISwitch *adSwitch;
 
 
 @end
@@ -25,46 +24,21 @@ bool switchEnabeled;
 }
 
 
-- (IBAction)adSwitch:(UISwitch *)sender {
-    switchEnabeled = "True";
-    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
-    [prefs setObject:@"True" forKey:@"switchStatus"];
-}
 
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
-    NSString *textToLoad = [prefs stringForKey:@"switchStatus"];
-    if ([textToLoad  isEqual: @"True"]) {
-        [_adSwitch setEnabled:(YES)];
-        
-    }
-    else {
-        [_adSwitch setEnabled:(NO)];
-        
-    }
+    // Replace this ad unit ID with your own ad unit ID.
+    self.bannerView.adUnitID = @"ca-app-pub-2616840641282201/7583672370";
+    self.bannerView.rootViewController = self;
     
-    
-    if (switchEnabeled == "True") {
-        // Replace this ad unit ID with your own ad unit ID.
-        self.bannerView.adUnitID = @"ca-app-pub-2616840641282201/7583672370";
-        self.bannerView.rootViewController = self;
-        
-        GADRequest *request = [GADRequest request];
-        // Requests test ads on devices you specify. Your test device ID is printed to the console when
-        // an ad request is made. GADBannerView automatically returns test ads when running on a
-        // simulator.
-        request.testDevices = @[ kGADSimulatorID ];
-        [self.bannerView loadRequest:request];
-    }
-    
-    else {
-        NSLog(@"No Ads");
-    }
-    
-    
+    GADRequest *request = [GADRequest request];
+    // Requests test ads on devices you specify. Your test device ID is printed to the console when
+    // an ad request is made. GADBannerView automatically returns test ads when running on a
+    // simulator.
+    request.testDevices = @[ kGADSimulatorID ];
+    [self.bannerView loadRequest:request];
 
 }
 
